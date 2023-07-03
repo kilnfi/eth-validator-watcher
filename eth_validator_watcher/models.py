f273ed13bd99a581542a8b216d87f4c8d9ade5ef
Manu NALEPA <emmanuel.nalepa@kiln.fi>
2023-07-03 10:32:14 +0200

    Models: Remove unused fields

--- eth_validator_watcher/models.py
+++ eth_validator_watcher/models.py
@@ -19,11 +19,6 @@ class Validators(BaseModel):
             withdrawalPossible = "withdrawal_possible"
             withdrawalDone = "withdrawal_done"
 
-            active = "active"
-            pending = "pending"
-            exited = "exited"
-            withdrawal = "withdrawal"
-
         class Validator(BaseModel):
             pubkey: str
             slashed: bool
@@ -39,8 +34,6 @@ class Validators(BaseModel):
 class Genesis(BaseModel):
     class Data(BaseModel):
         genesis_time: int
-        genesis_validators_root: str
-        genesis_fork_version: str
 
     data: Data
 
@@ -49,64 +42,27 @@ class Block(BaseModel):
     class Data(BaseModel):
         class Message(BaseModel):
             class Body(BaseModel):
-                class Eth1Data(BaseModel):
-                    deposit_root: str
-                    deposit_count: int
-                    block_hash: str
-
                 class Attestation(BaseModel):
                     class Data(BaseModel):
-                        class Source(BaseModel):
-                            epoch: int
-                            root: str
-
-                        class Target(BaseModel):
-                            epoch: int
-                            root: str
-
                         slot: int
                         index: int
-                        beacon_block_root: str
-                        source: Source
-                        target: Target
 
                     aggregation_bits: str
                     data: Data
-                    signature: str
 
                 class ExecutionPayload(BaseModel):
-                    parent_hash: str
                     fee_recipient: str
-                    state_root: str
-                    receipts_root: str
-                    logs_bloom: str
-                    prev_randao: str
-                    block_number: int
-                    gas_limit: int
-                    timestamp: int
-                    extra_data: str
-                    base_fee_per_gas: int
                     block_hash: str
-                    transactions: list[str]
 
-                randao_reveal: str
-                eth1_data: Eth1Data
-                graffiti: str
-                proposer_slashings: list[int]
-                attester_slashings: list[int]
                 attestations: list[Attestation]
                 execution_payload: ExecutionPayload
 
             slot: int
             proposer_index: int
-            parent_root: str
-            state_root: str
             body: Body
 
         message: Message
 
-    version: str
-    execution_optimistic: bool
     data: Data
 
 
@@ -116,7 +72,6 @@ class Committees(BaseModel):
         slot: int
         validators: list[int]
 
-    execution_optimistic: bool
     data: list[Data]
 
 
@@ -168,10 +123,10 @@ class BeaconType(str, Enum):
 
 
 class EthGetBlockByHashRequest(BaseModel):
-    jsonrpc = "2.0"
-    method = "eth_getBlockByHash"
+    jsonrpc: str = "2.0"
+    method: str = "eth_getBlockByHash"
     params: list
-    id = "1"
+    id: str = "1"
 
 
 class ExecutionBlock(BaseModel):
@@ -184,3 +139,7 @@ class ExecutionBlock(BaseModel):
     jsonrpc: str
     id: int
     result: Result
+
+
+class Header(BaseModel):
+    version: str
