7f3e774b55554d775a923296ea945fd094aeb201
Manu NALEPA <emmanuel.nalepa@kiln.fi>
2023-10-25 11:09:01 +0200

    Fix crash at watcher start

--- tests/beacon/test_get_validators_liveness.py
+++ tests/beacon/test_get_validators_liveness.py
@@ -1,14 +1,15 @@
 import json
 from pathlib import Path
 
-from requests_mock import Mocker
-
 from eth_validator_watcher.beacon import Beacon
 from eth_validator_watcher.models import BeaconType
+from pytest import raises
+from requests import HTTPError, Response, codes
+from requests_mock import Mocker
 from tests.beacon import assets
 
 
-def test_get_proposer_duties_lighthouse():
+def test_get_validators_liveness_lighthouse():
     beacon_url = "http://beacon:5052"
 
     liveness_request_path = (
@@ -45,7 +46,7 @@ def test_get_proposer_duties_lighthouse():
         )
 
 
-def test_get_proposer_duties_nimbus():
+def test_get_validators_liveness_nimbus():
     beacon_url = "http://beacon:5052"
     beacon = Beacon(beacon_url)
 
@@ -54,7 +55,7 @@ def test_get_proposer_duties_nimbus():
     ) == {42: True, 44: True, 46: True}
 
 
-def test_get_proposer_duties_teku():
+def test_get_validators_liveness_teku():
     beacon_url = "http://beacon:5052"
 
     liveness_request_path = Path(assets.__file__).parent / "liveness_request_teku.json"
@@ -88,7 +89,7 @@ def test_get_proposer_duties_teku():
         )
 
 
-def test_get_proposer_duties_beacon_api():
+def test_get_validators_liveness_beacon_api():
     beacon_url = "http://beacon:5052"
 
     liveness_request_path = (
@@ -120,4 +121,43 @@ def test_get_proposer_duties_beacon_api():
                 beacon_type=BeaconType.OTHER, epoch=1664, validators_index={42, 44, 46}
             )
             == expected
+        )
+
+
+def test_get_validators_liveness_beacon_api_bad_request():
+    beacon_url = "http://beacon:5052"
+
+    def post(url: str, **_) -> Response:
+        assert url == f"{beacon_url}/eth/v1/validator/liveness/1664"
+        response = Response()
+        response.status_code = codes.bad_request
+
+        return response
+
+    beacon = Beacon(beacon_url)
+    beacon._Beacon__http_retry_not_found.post = post  # type: ignore
+
+    expected = {42: True, 44: True, 46: True}
+
+    assert expected == beacon.get_validators_liveness(
+        beacon_type=BeaconType.OTHER, epoch=1664, validators_index={42, 44, 46}
+    )
+
+
+def test_get_validators_liveness_beacon_api_no_extended():
+    beacon_url = "http://beacon:5052"
+
+    def post(url: str, **_) -> Response:
+        assert url == f"{beacon_url}/eth/v1/validator/liveness/1664"
+        response = Response()
+        response.status_code = codes.not_extended
+
+        return response
+
+    beacon = Beacon(beacon_url)
+    beacon._Beacon__http_retry_not_found.post = post  # type: ignore
+
+    with raises(HTTPError):
+        beacon.get_validators_liveness(
+            beacon_type=BeaconType.OTHER, epoch=1664, validators_index={42, 44, 46}
         )
