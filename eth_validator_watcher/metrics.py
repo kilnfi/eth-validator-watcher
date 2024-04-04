2d6dfdbf374f64f64d45d9f6a2718e876e3aa99c
s. rannou <mxs@sbrk.org>
2024-04-04 18:01:24 +0200

    feat: compute ideal/actual consensus rewards per scope

--- eth_validator_watcher/metrics.py
+++ eth_validator_watcher/metrics.py
@@ -19,6 +19,9 @@ class PrometheusMetrics:
     eth_suboptimal_sources_rate: Gauge
     eth_suboptimal_targets_rate: Gauge
     eth_suboptimal_heads_rate: Gauge
+    eth_ideal_consensus_rewards: Gauge
+    eth_actual_consensus_rewards: Gauge
+    eth_consensus_rewards_rate: Gauge
 
 
 def get_prometheus_metrics() -> PrometheusMetrics:
@@ -38,6 +41,9 @@ def get_prometheus_metrics() -> PrometheusMetrics:
             eth_suboptimal_sources_rate=Gauge("eth_suboptimal_sources_rate", "Suboptimal sources rate", ['scope']),
             eth_suboptimal_targets_rate=Gauge("eth_suboptimal_targets_rate", "Suboptimal targets rate", ['scope']),
             eth_suboptimal_heads_rate=Gauge("eth_suboptimal_heads_rate", "Suboptimal heads rate", ['scope']),
+            eth_ideal_consensus_rewards=Gauge("eth_ideal_consensus_rewards", "Ideal consensus rewards", ['scope']),
+            eth_actual_consensus_rewards=Gauge("eth_actual_consensus_rewards", "Actual consensus rewards", ['scope']),
+            eth_consensus_rewards_rate=Gauge("eth_consensus_rewards_rate", "Consensus rewards rate", ['scope']),
         )
 
     return _metrics
