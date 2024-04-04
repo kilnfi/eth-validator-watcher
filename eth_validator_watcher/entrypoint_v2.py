9865864999cf067b7dde9059a6f2a85516c40188
s. rannou <mxs@sbrk.org>
2024-04-04 13:30:07 +0200

    feat: add basic handling of the main processing loop and introduced watched validators

--- eth_validator_watcher/entrypoint_v2.py
+++ eth_validator_watcher/entrypoint_v2.py
@@ -5,13 +5,15 @@ from functools import partial
 from pathlib import Path
 from typing import Optional
 
+import logging
 import typer
 import time
 
+from .beacon import Beacon
 from .config import load_config, WatchedKeyConfig
+from .watched_validators import WatchedValidators
 
 
-print = partial(print, flush=True)
 app = typer.Typer(add_completion=False)
 
 
@@ -27,23 +29,37 @@ class ValidatorWatcher:
         cfg_path: Path
             Path to the configuration file.
         """
-        self.cfg_path = cfg_path
-        self.cfg = None
+        self._cfg_path = cfg_path
+        self._cfg = None
+        self._beacon = None
 
-    def reload_config(self) -> None:
+        self._reload_config()
+
+    def _reload_config(self) -> None:
         """Reload the configuration file.
         """
         try:
-            self.cfg = load_config(self.cfg_path)
+            self._cfg = load_config(self._cfg_path)
         except ValidationError as err:
             raise typer.BadParameter(f'Invalid configuration file: {err}')
+
+        if self._beacon is None or self._beacon.get_url() != self._cfg.beacon_url or self._beacon.get_timeout_sec() != self._cfg.beacon_timeout_sec:
+            self._beacon = Beacon(self._cfg.beacon_url, self._cfg.beacon_timeout_sec)
 
     def run(self) -> None:
         """Run the Ethereum Validator Watcher.
         """
+        watched_validators = WatchedValidators()
         while True:
-            print('Reloading configuration...')
-            self.reload_config()
+            logging.info('Processing new epoch')
+            beacon_validators = self._beacon.get_validators()
+            watched_validators.process_epoch(beacon_validators)
+
+            logging.info('Processing configuration update')
+            self._reload_config()
+            watched_validators.process_config(self._cfg)
+
+            logging.info('Waiting for next iteration')
             time.sleep(1)
 
 
@@ -60,6 +76,10 @@ def handler(
     ),
 ) -> None:
     """Run the Ethereum Validator Watcher."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)-8s %(message)s'
+    )
 
     watcher = ValidatorWatcher(config)
     watcher.run()
