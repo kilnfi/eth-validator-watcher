b4ff48bc5d75686f4c7392612ee46c8b16c8f7f5
s. rannou <mxs@sbrk.org>
2024-05-13 13:01:16 +0200

    feat: strip down beacon class to the minimum

--- eth_validator_watcher/beacon.py
+++ eth_validator_watcher/beacon.py
@@ -1,19 +1,15 @@
 """Contains the Beacon class which is used to interact with the consensus layer node."""
 
 import functools
-from collections import defaultdict
-from typing import Any, Optional, Union
+from typing import Any, Union
 
 from requests import HTTPError, Response, Session, codes
 from requests.adapters import HTTPAdapter, Retry
-from requests.exceptions import ChunkedEncodingError, RetryError
+from requests.exceptions import ChunkedEncodingError
 from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 from .models import (
-    BeaconType,
-    Block,
     BlockIdentierType,
-    Committees,
     Genesis,
     Header,
     ProposerDuties,
@@ -155,30 +151,6 @@ class Beacon:
         header_dict = response.json()
         return Header(**header_dict)
 
-    def get_block(self, slot: int) -> Block:
-        """Get a block.
-
-        Parameters
-        slot: Slot corresponding to the block to retrieve
-        """
-        try:
-            response = self.__get(
-                f"{self.__url}/eth/v2/beacon/blocks/{slot}", timeout=self.__timeout_sec
-            )
-
-            response.raise_for_status()
-
-        except HTTPError as e:
-            if e.response.status_code == codes.not_found:
-                # If we are here, it means the block does not exist
-                raise NoBlockError from e
-
-            # If we are here, it's an other error
-            raise
-
-        block_dict = response.json()
-        return Block(**block_dict)
-
     def get_proposer_duties(self, epoch: int) -> ProposerDuties:
         """Get proposer duties
 
@@ -192,32 +164,6 @@ class Beacon:
 
         proposer_duties_dict = response.json()
         return ProposerDuties(**proposer_duties_dict)
-
-    def get_status_to_index_to_validator(
-        self,
-    ) -> dict[StatusEnum, dict[int, Validators.DataItem.Validator]]:
-        """Get a nested dictionnary with:
-        outer key               : Status
-        outer value (=inner key): Index of validator
-        inner value             : Validator
-        """
-        response = self.__get_retry_not_found(
-            f"{self.__url}/eth/v1/beacon/states/head/validators", timeout=self.__timeout_sec
-        )
-
-        response.raise_for_status()
-        validators_dict = response.json()
-
-        validators = Validators(**validators_dict)
-
-        result: dict[
-            StatusEnum, dict[int, Validators.DataItem.Validator]
-        ] = defaultdict(dict)
-
-        for item in validators.data:
-            result[item.status][item.index] = item.validator
-
-        return result
 
     def get_validators(self, slot: int) -> Validators:
         response = self.__get_retry_not_found(
@@ -233,38 +179,6 @@ class Beacon:
         del validators_dict
 
         return validators
-
-    def get_duty_slot_to_committee_index_to_validators_index(
-        self, epoch: int
-    ) -> dict[int, dict[int, list[int]]]:
-        """Get a nested dictionnary.
-        outer key               : Slot number
-        outer value (=inner key): Committee index
-        inner value             : Index of validators that have to attest in the
-                                  given committee index at the given slot
-
-        Parameters:
-        epoch: Epoch
-        """
-        response = self.__get_retry_not_found(
-            f"{self.__url}/eth/v1/beacon/states/head/committees",
-            params=dict(epoch=epoch),
-            timeout=self.__timeout_sec,
-        )
-
-        response.raise_for_status()
-        committees_dict = response.json()
-
-        committees = Committees(**committees_dict)
-        data = committees.data
-
-        # TODO: Do it with dict comprehension
-        result: dict[int, dict[int, list[int]]] = defaultdict(dict)
-
-        for item in data:
-            result[item.slot][item.index] = item.validators
-
-        return result
 
     def get_rewards(self, epoch: int) -> Rewards:
         """Get rewards.
@@ -303,31 +217,3 @@ class Beacon:
         validators_liveness = ValidatorsLivenessResponse(**validators_liveness_dict)
         del validators_liveness_dict
         return validators_liveness
-
-    def get_potential_block(self, slot) -> Block | None:
-        """Get a block if it exists, otherwise return None.
-
-        Parameters:
-        slot: Slot corresponding to the block to retrieve
-        """
-        try:
-            return self.get_block(slot)
-        except NoBlockError:
-            # The block is probably orphaned:
-            # The beacon saw the block (that's why we received the event) but it was
-            # orphaned before we could fetch it.
-            return None
-
-    def __get_validators_liveness_beacon_api(
-        self, epoch: int, validators_index: set[int]
-    ) -> Response:
-        """Get validators liveness from neither Lighthouse nor Teku.
-
-        https://github.com/ConsenSys/teku/issues/7204
-
-        Parameters:
-        epoch           : Epoch corresponding to the validators liveness to retrieve
-        validators_index: Set of validator indexs corresponding to the liveness to
-                          retrieve
-        """
-        return 
