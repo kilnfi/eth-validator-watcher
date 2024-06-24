767b0e1050328daddb72116705aee64d4f8a9ca8
s. rannou <mxs@sbrk.org>
2024-06-24 16:03:20 +0200

    feat: adapt Dockerfile to cpp builds

---
+++ build.py
@@ -0,0 +1,19 @@
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+
+def build(setup_kwargs):
+    ext_modules = [
+        Pybind11Extension(
+            "eth_validator_watcher_ext", [
+                "eth_validator_watcher/helpers.cc"
+            ],
+            extra_compile_args=['-O3', '-pthread'],
+            language='c++',
+            cxx_std=17
+        )
+    ]
+    setup_kwargs.update({
+        "ext_modules": ext_modules,
+        "cmd_class": {"build_ext": build_ext},
+        "zip_safe": False,
+    })
