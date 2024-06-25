e5bb2eb068afbf74bb3fc0cf1321f79739454263
s. rannou <mxs@sbrk.org>
2024-06-25 11:39:12 +0200

    fix: config issue with default values

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,20 +9,24 @@ def test_null_config() -> None:
     path = str(Path(assets.__file__).parent / "config.null.yaml")
     config = load_config(path)
 
-    assert config.beacon_url is None
+    assert config.beacon_url == 'http://localhost:5051/'
+    assert config.metrics_port == 8000
     assert config.beacon_timeout_sec == 90
-    assert config.network is None
-    assert config.watched_keys is None
+    assert config.network == 'mainnet'
+
+    assert config.watched_keys == []
 
 
 def test_empty_config() -> None:
     path = str(Path(assets.__file__).parent / "config.empty.yaml")
     config = load_config(path)
 
-    assert config.beacon_url is None
+    assert config.beacon_url == 'http://localhost:5051/'
     assert config.beacon_timeout_sec == 90
-    assert config.network is None
-    assert config.watched_keys is None
+    assert config.metrics_port == 8000
+    assert config.network == 'mainnet'
+
+    assert config.watched_keys == []
 
 
 def test_filled_config() -> None:
@@ -31,6 +35,7 @@ def test_filled_config() -> None:
 
     assert config.beacon_url == 'http://localhost:5051/'
     assert config.beacon_timeout_sec == 90
+    assert config.metrics_port == 4242
     assert config.network == 'holesky'
 
     assert [k.public_key for k in config.watched_keys] == ['0x832b8286f5d6535fd941c6c4ed8b9b20d214fc6aa726ce4fba1c9dbb4f278132646304f550e557231b6932aa02cf08d3']
