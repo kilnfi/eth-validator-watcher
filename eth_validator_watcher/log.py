d5bb001a6accace3479c08d099a62085bb3b2a9f
s. rannou <mxs@sbrk.org>
2024-06-26 18:28:12 +0200

    fix: nit

--- eth_validator_watcher/log.py
+++ eth_validator_watcher/log.py
@@ -25,6 +25,18 @@ def shorten_validator(validator_pubkey: str) -> str:
     return f"{validator_pubkey[:10]}"
 
 
+def beaconcha_validator_link(cfg: Config, validator: str) -> str:
+    """Return a link to the beaconcha.in validator page.
+    """
+    return f'<https://{cfg.network}.beaconcha.in/validator/{validator}|{shorten_validator(validator)}>'
+
+
+def beaconcha_slot_link(cfg: Config, slot: int) -> str:
+    """Return a link to the beaconcha.in slot page.
+    """
+    return f'<https://{cfg.network}.beaconcha.in/slot/{slot}|{slot}>'
+
+
 def slack_send(cfg: Config, msg: str) -> None:
     """Attempts to send a message to the configured slack channel."""
     if not (cfg.slack_channel and cfg.slack_token):
@@ -37,26 +49,27 @@ def slack_send(cfg: Config, msg: str) -> None:
         logging.warning(f'😿 Unable to send slack notification: {e.response["error"]}')
 
 
-
-def log_single_entry(cfg: Config, validator: str, registry: WatchedValidators, msg: str, emoji: str, color: str) -> None:
+def log_single_entry(cfg: Config, validator: str, registry: WatchedValidators, msg: str, emoji: str, slot: int, color: str) -> None:
     """Logs a single validator entry.
     """
     v = registry.get_validator_by_pubkey(validator)
 
-    label_msg = ''
+    label_msg_shell = ''
+    label_msg_slack = ''
     if v:
         labels = [label for label in v.labels if not label.startswith('scope:')]
         if labels:
-            label_msg = f' ({", ".join(labels)})'
+            label_msg_slack = f' ({", ".join([f"`{l}`" for l in labels])})'
+            label_msg_shell = f' ({", ".join(labels)})'
 
-    msg_slack = f'{emoji} Validator {shorten_validator(validator)}{label_msg} {msg}'
-    msg_shell = f'{color}{msg_slack}{COLOR_RESET}'
-
+    msg_shell = f'{color}{emoji} Validator {shorten_validator(validator)}{label_msg_shell} {msg}{COLOR_RESET}'
     logging.info(msg_shell)
+
+    msg_slack = f'{emoji} Validator {beaconcha_validator_link(cfg, validator)}{label_msg_slack} {msg} on slot {beaconcha_slot_link(cfg, slot)}'
     slack_send(cfg, msg_slack)
 
 
-def log_multiple_entries(cfg: Config, validators: list[str], registry: WatchedValidators, msg: str, emoji: str, color: str) -> None:
+def log_multiple_entries(cfg: Config, validators: list[str], registry: WatchedValidators, msg: str, emoji: str, slot: int, color: str) -> None:
     """Logs a multiple validator entries.
     """
 
@@ -73,12 +86,12 @@ def log_multiple_entries(cfg: Config, validators: list[str], registry: WatchedVa
     if top_labels:
         label_msg = f' ({", ".join(top_labels)}...)'
 
-    msg_validators = f'{", ".join([shorten_validator(v) for v in validators])} and more'
-
-    msg_slack = f'{emoji} Validator(s) {msg_validators}{label_msg} {msg}'
-    msg_shell = f'{color}{msg_slack}{COLOR_RESET}'
-
+    msg_validators_shell = f'{", ".join([shorten_validator(v) for v in validators])} and more'
+    msg_shell = f'{color}{emoji} Validator(s) {msg_validators}{label_msg} {msg} on slot {slot}{COLOR_RESET}'
     logging.info(msg_shell)
+
+    msg_validators_slack = f'{", ".join([beaconcha_validator_link(cfg, v) for v in validators])} and more'
+    msg_slack = f'{emoji} Validator(s) {msg_validators}{label_msg} {msg} on slot {beaconcha_slot_link(cfg, slot)}'
     slack_send(cfg, msg_slack)
 
 
@@ -91,20 +104,17 @@ def log_details(cfg: Config, registry: WatchedValidators, metrics: MetricsByLabe
 
     for slot, validator in m.details_future_blocks:
         # Only log once per epoch future block proposals.
-        if current_slot % 32 == 0:
-            log_single_entry(cfg, validator, registry, f'will propose a block on slot {slot}', '🙏', COLOR_GREEN)
+        if current_slot % 32 == 0 and slot >= current_slot + 32:
+            log_single_entry(cfg, validator, registry, f'will propose a block', '🙏', slot, COLOR_GREEN)
 
     for slot, validator in m.details_proposed_blocks:
-        log_single_entry(cfg, validator, registry, f'proposed a block on slot {slot}', '🏅', COLOR_BOLD_GREEN)
+        log_single_entry(cfg, validator, registry, f'proposed a block', '🏅', slot, COLOR_BOLD_GREEN)
 
     for slot, validator in m.details_missed_blocks:
-        log_single_entry(cfg, validator, registry, f'likely missed a block on slot {slot}', '😩', COLOR_RED)
+        log_single_entry(cfg, validator, registry, f'likely missed a block', '😩', slot, COLOR_RED)
 
     for slot, validator in m.details_missed_blocks_finalized:
-        log_single_entry(cfg, validator, registry, f'missed a block for real on slot {slot}', '😭', COLOR_BOLD_RED)
-
-    for validator in m.details_missed_attestations:
-        log_single_entry(cfg, validator, registry, f'missed a block for real on slot {slot}', '😭', COLOR_BOLD_RED)        
+        log_single_entry(cfg, validator, registry, f'missed a block for real', '😭', slot, COLOR_BOLD_RED)
 
     if m.details_missed_attestations:
-        log_multiple_entries(cfg, m.details_missed_attestations, registry, f'missed an attestation', '😞', COLOR_YELLOW)
+        log_multiple_entries(cfg, m.details_missed_attestations, registry, f'missed an attestation', '😞', slot, COLOR_YELLOW)
